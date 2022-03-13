2bdebc4c03efd04f9a8584ca6547d19523160488
YohanesGetinet1 <johnget496@gmail.com>
2022-03-13 12:08:40 +0530

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  *main - entry point
  *
- *Return: 0 after printing the function
+ *Return: 0 after printing the function 
  */
 int main(void)
 {
-puts("\"Programming is like building a multlingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
