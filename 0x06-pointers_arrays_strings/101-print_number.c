f9cf340600b922f15f9f7188f0ed2da465cbe7c1
YohanesGetinet1 <johnget496@gmail.com>
2022-03-23 16:42:34 +0530

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_number - prints number
+ * @n: the number to process.
+ *
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 10 && n >= 0)
+	{
+		_putchar('0' + n);
+		return;
+	}
+	if (n < 0)
+	{
+		num = -n;
+		_putchar('-');
+	}
+	else
+	{
+		num = n;
+	}
+
+	if (num >= 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar('0' + (num % 10));
+}
