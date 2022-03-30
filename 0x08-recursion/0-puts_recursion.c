71e66ad1c419fab4aa06372dbdf1d5c6bd3a3760
YohanesGetinet1 <johnget496@gmail.com>
2022-03-30 14:02:11 +0530

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _puts_recursion - Print a string
+*@s: char received
+*Return: Nothing
+*/
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
