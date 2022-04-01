6aab23a415e119232d6a8d37bdbb305b9fde04ab
YohanesGetinet1 <johnget496@gmail.com>
2022-04-01 10:17:49 +0530

    main

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+int _atoi(char *s);
+int _isdigit(char *c);
+
+#endif /* _SAMPLE_HEADER_H_ */
