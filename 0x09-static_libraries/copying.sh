0713220e003bcdcf016bf013df427f72d13867fb
YohanesGetinet1 <johnget496@gmail.com>
2022-04-05 12:21:01 +0530

    list

---
+++ 0x09-static_libraries/copying.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cp ./alxschool-low_level_programming/0x04-more_functions_nested_loops/0-isupper.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x07-pointers_arrays_strings/0-memset.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x06-pointers_arrays_strings/0-strcat.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x04-more_functions_nested_loops/1-isdigit.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x07-pointers_arrays_strings/1-memcpy.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x06-pointers_arrays_strings/1-strncat.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x07-pointers_arrays_strings/2-strchr.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x05-pointers_arrays_strings/2-strlen.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x06-pointers_arrays_strings/2-strncpy.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x02-functions_nested_loops/3-islower.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x05-pointers_arrays_strings/3-puts.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x06-pointers_arrays_strings/3-strcmp.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x02-functions_nested_loops/4-isalpha.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x02-functions_nested_loops/6-abs.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x05-pointers_arrays_strings/9-strcpy.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x02-functions_nested_loops/_putchar.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x04-more_functions_nested_loops/_putchar.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x05-pointers_arrays_strings/_putchar.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+cp ./alxschool-low_level_programming/0x06-pointers_arrays_strings/_putchar.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
