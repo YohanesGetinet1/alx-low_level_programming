debba7bb6ca489ee9284a93e800a15267001a480
YohanesGetinet1 <johnget496@gmail.com>
2022-04-11 14:47:14 +0530

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints name ofthe file it was compiled from
+ * Return: (0)
+ */
+
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
