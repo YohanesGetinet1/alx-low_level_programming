2dce95977230f13a404c6c2394cab3134e28e3a4
YohanesGetinet1 <johnget496@gmail.com>
2022-05-03 16:20:48 +0530

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,36 @@
 #include "main.h"
 
 /**
-* read_textFile -Reads a text file and prints in POSIX stdout.
-* @filename: file 2 read.
+* read_textfile - Reads a text file and prints in POSIX stdout.
+* @filename: File 2 read.
 * @letters: Number of letter it should read and print.
 * Return: Actual number of letter it could read and print.
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
-{
 
+{
 int fdest, size;
 char *buffer;
-buffer = malloc(sizeod(char)* letters);
+buffer = malloc(sizeof(char) * letters);
 
 /*Check content file*/
 
 if (!filename)
-return(0);
+return (0);
+/*Check value of buffer*/
 
-/*Check the value of buffer*/
-
-if(!buffer)
+if (!buffer)
 return (0);
 
-/*open file*/
-
-fdes = open(filename, O_RODONLY);
-
+/*Open file*/
+fdest = open(filename, O_RDONLY);
 /*Read file*/
 
-size = write (STDOUT_FILENO, buffer, read(fdest, buffer, letters));
+size = write(STDOUT_FILENO, buffer, read(fdest, buffer, letters));
 
-if(fdest == -1 || size == -1)
+if (fdest == -1 || size == -1)
+
 return (0);
 
 close(fdest);
@@ -41,4 +39,3 @@ free(buffer);
 
 return (size);
 }
-
